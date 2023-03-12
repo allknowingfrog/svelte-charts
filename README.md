ab6168b2879321b33c81c1a040151c2cb947e1d0
allknowingfrog <jeremiah.bohling@gmail.com>
2023-03-12 12:19:25 -0500

    Initialize Svelte app

---
+++ README.md
@@ -0,0 +1,21 @@
+# To Do
+
+Demo Chart UI.
+
+## Setup
+
+Install dependencies.
+```
+npm install
+```
+
+## Development
+
+To launch local development server:
+```
+npm run dev
+```
+
+## Resources
+
+`chart.svg` is an image from https://www.svgrepo.com.
---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <img src="/chart.svg" class="logo" alt="Vite Logo" />
+  </div>
+  <h1>Svelte Charts</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
